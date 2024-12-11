5e239c320244bfca88ace78438c3fa19b0e22920
Fael HENINE <fael.henine@gmail.com>
2024-12-11 08:29:46 +0100

    ajout de la fonction de création de la bdd

--- database.py
+++ database.py
@@ -1,1 +1,16 @@
-print("hello world")
+import sqlite3
+
+def new_db():
+    connection = sqlite3.connect("sql.db")
+    cursor = connection.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS content (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            critere1 TEXT NOT NULL,
+            critere2 TEXT,
+            critere3 TEXT
+        )
+    """)
+    connection.commit()
+    connection.close()
+    print("Création de la base de données fini.")
